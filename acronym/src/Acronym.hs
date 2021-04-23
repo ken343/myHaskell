16b4a013b28107722bf5ed5186fa5a0002c381fc
ken343 <ken@dealwire.me>
2021-04-23 00:47:47 -0500

    No Change to Guards Yet with Function

---
+++ acronym/src/Acronym.hs
@@ -0,0 +1,35 @@
+module Acronym (abbreviate) where
+import qualified Data.Text as T
+import           Data.Text (Text)
+
+-- I do not trust my understanding of the '$' operator's precedence/relationship
+-- with ':' or infix functions to try and clean up some of this code.
+grabLetters :: String -> String -> String
+grabLetters [] acc = acc
+grabLetters (s:ss) acc
+  | s `elem` separators = if (head ss) `elem` separators then grabLetters ss acc else grabLetters (tail ss) ((head ss) : acc)
+  | s `elem` capitals && (if null acc then False else (head acc) `elem` capitals) = grabLetters ss acc
+  | s `elem` capitals = grabLetters ss (s : acc) 
+  | otherwise = grabLetters ss acc
+  where separators = [' ', '-', '_']
+        capitals = ['A'..'Z']
+
+
+multiCapCheck = String -> String -> Bool
+multiCapCheck xs [] = False
+multiCapCheck (x:xs) (y:ys) -- Do I add a flag here to indicate detection of consecutive capital letters?
+  | x `elem` capitals && y `elem` capitals = True
+  where capitals = ['A'..'Z']
+
+-- grabLetters will not grab the first letter of the String if it is lowercase.
+-- firstLetter will extract it in this event.
+firstLetter :: String -> String
+firstLetter (s:ss)
+  | s `elem` ['a'..'z'] = [s]
+  | otherwise = []
+
+orientString :: String -> String
+orientString ss = T.unpack . T.toUpper . T.pack $ reverse ss
+
+abbreviate :: String -> String
+abbreviate xs = orientString $ (grabLetters xs []) ++ (firstLetter xs)
