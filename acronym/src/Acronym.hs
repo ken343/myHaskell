a2c4049704e9e3121c7a581b3ebeb58ace5c3c9a
ken343 <ken@dealwire.me>
2021-04-23 01:16:16 -0500

    Git Ignoring

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -6,19 +6,20 @@ import           Data.Text (Text)
 -- with ':' or infix functions to try and clean up some of this code.
 grabLetters :: String -> String -> String
 grabLetters [] acc = acc
-grabLetters (s:ss) acc
+grabLetters all@(s:ss) acc
   | s `elem` separators = if (head ss) `elem` separators then grabLetters ss acc else grabLetters (tail ss) ((head ss) : acc)
-  | s `elem` capitals && (if null acc then False else (head acc) `elem` capitals) = grabLetters ss acc
+  | multiCapCheck all acc = grabLetters ss acc
   | s `elem` capitals = grabLetters ss (s : acc) 
   | otherwise = grabLetters ss acc
   where separators = [' ', '-', '_']
         capitals = ['A'..'Z']
 
 
-multiCapCheck = String -> String -> Bool
+multiCapCheck :: String -> String -> Bool
 multiCapCheck xs [] = False
 multiCapCheck (x:xs) (y:ys) -- Do I add a flag here to indicate detection of consecutive capital letters?
   | x `elem` capitals && y `elem` capitals = True
+  | otherwise = False
   where capitals = ['A'..'Z']
 
 -- grabLetters will not grab the first letter of the String if it is lowercase.
