e5c1654958a0c80393939ee9bf2fcfad859dba0c
ken343 <ken@dealwire.me>
2021-04-27 18:46:08 -0500

    Sovled Straining Problem

--- strain/src/Strain.hs
+++ strain/src/Strain.hs
@@ -1,7 +1,13 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p xs = error "You need to implement this function."
+discard _ [] = []
+discard p (x:xs) 
+    | p x = discard p xs
+    | otherwise = x:discard p xs
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep p xs = error "You need to implement this function."
+keep _ [] = []
+keep p (x:xs) 
+    | p x = x:keep p xs
+    | otherwise = keep p xs
